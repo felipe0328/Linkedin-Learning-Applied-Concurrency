f04d7f304cea94a67ff74c47ef1c061cfbdb4ed4
Felipe Puentes <feliipepuentes@gmail.com>
2023-02-27 20:23:45 +0000

    Removing folders to inmplement the new endpoints methodology

--- main.go
+++ main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"appliedConcurrency/basics"
-	"appliedConcurrency/channels"
 	"appliedConcurrency/colors"
-	"appliedConcurrency/patterns"
 	"fmt"
 )
 
 func main() {
 	fmt.Printf("----\n \t%s\n----\n\n", colors.SprintFColor(colors.Red, "Go Concurrency Course"))
-	basics.RunBasics()
-	channels.RunChannels()
-	patterns.RunPatterns()
 }
--- patterns/main.go
+++
@@ -1,5 +0,0 @@
-package patterns
-
-func RunPatterns() {
-
-}
